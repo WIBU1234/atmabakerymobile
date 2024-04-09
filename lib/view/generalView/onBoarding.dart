5854189282556fea59893f8f92cf80d02893a90c
WIBU1234 <210711186@students.uajy.ac.id>
2024-04-09 13:49:56 +0700

    OnBoardingViewProgress(SayaMauNgeBranch)

--- lib/view/generalView/onBoarding.dart
+++ lib/view/generalView/onBoarding.dart
@@ -15,23 +15,56 @@ class _OnBoardingState extends State<OnBoarding> {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: 
-      
-      
-      Container(
+      body: Container(
         width: screenWidth,
-        height: screenHeight,      
-        decoration: BoxDecoration(
-          color: const Color(0xFF131313).withOpacity(0.41),          
+        height: screenHeight,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/Cupbrownies.jpeg'),
+            fit: BoxFit.cover,
+          ),
         ),
 
         child: Container(
           width: screenWidth,
           height: screenHeight,      
           decoration: BoxDecoration(
-            color: const Color(0xFF000000).withOpacity(0.16),          
+            color: const Color(0xFF131313).withOpacity(0.42),          
           ),
-        ),  
+
+          child: Container(
+            width: screenWidth,
+            height: screenHeight,      
+            decoration: BoxDecoration(
+              color: const Color(0xFF000000).withOpacity(0.35),          
+            ),
+
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: screenHeight * 0.12),
+
+                  child: Container(
+                    width: screenWidth * 0.86,
+                    height: screenHeight * 0.2,
+                    color: Colors.white,
+                  ),
+
+                  
+                  // child: const Text(
+                  //   'Asu Koe',
+                  //   style: TextStyle(
+                  //     color: Colors.white
+                  //   ),
+                  // ),
+                ),
+              ],
+            ),
+
+          ),
+        )        
       )
     );
   }
