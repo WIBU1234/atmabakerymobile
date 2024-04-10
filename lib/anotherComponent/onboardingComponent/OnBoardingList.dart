132e800f34fff2395d2779f3a98ef6f01b1ab61c
WIBU1234 <210711186@students.uajy.ac.id>
2024-04-10 18:45:43 +0700

    BoardingPageKelar

---
+++ lib/anotherComponent/onboardingComponent/OnBoardingList.dart
@@ -0,0 +1,26 @@
+import 'package:atmabakerymobile/anotherComponent/onboardingComponent/OnBoardingModel.dart';
+
+class OnBoardingItems{
+  List<OnBoarding_Info> items = [
+    OnBoarding_Info(
+      title: 'Rasakan kelezatan tiada tara, di Atma Bakery',
+      desc1: 'Besk Cake All Over Babarsari',
+      desc2: 'Region',
+      image: 'assets/images/Cupbrownies.jpeg',
+    ),
+
+    OnBoarding_Info(
+      title: 'Anda dapat melakukan pemesanan kapanpun dimanapun',
+      desc1: 'Tersedia aneka varian cake',
+      desc2: 'terkenal!!!',
+      image: 'assets/images/Triple Chocolate Muffins.jpeg',
+    ),
+
+    OnBoarding_Info(
+      title: 'Lakukan pemesanan Anda hanya di Atma Bakery',
+      desc1: 'Reach us soon',
+      desc2: '',
+      image: 'assets/images/Fudgy Brown Butter Brownies - Butternut Bakery.jpeg',
+    ),
+  ];
+}
---
+++ lib/anotherComponent/onboardingComponent/OnBoardingModel.dart
@@ -0,0 +1,8 @@
+class OnBoarding_Info {
+  String title;
+  String desc1;
+  String? desc2;
+  String image;
+
+  OnBoarding_Info({required this.title, required this.desc1, this.desc2, required this.image});
+}
